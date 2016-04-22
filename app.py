d913fec631a1e297d02b40a0311fe6428590c7e5
aurigadl <aurigadl@gmail.com>
2016-04-22 12:21:29 -0500

    Add role

--- app.py
+++ app.py
@@ -1,44 +1,104 @@
 from datetime import datetime, timedelta
 import os
-import jwt
-import json
-import requests
-import base64
 from functools import wraps
-from urlparse import parse_qs, parse_qsl
-from urllib import urlencode
-from flask import Flask, g, send_file, request, redirect, url_for, jsonify
+from flask import Flask, g, request, jsonify, current_app
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from requests_oauthlib import OAuth1
 from jwt import DecodeError, ExpiredSignature
+from flask.ext.rbac import RBAC, RoleMixin, UserMixin
+
 
 # Configuration
-
 current_path = os.path.dirname(__file__)
 client_path = os.path.abspath(os.path.join(current_path, '..', '..', 'client'))
 
 app = Flask(__name__)
 app.config.from_object('config')
-
+rbac = RBAC(app)
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+# A base model for other database tables to inherit
+class Base(db.Model):
+    __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    display_name = db.Column(db.String(120))
-    facebook = db.Column(db.String(120))
-    github = db.Column(db.String(120))
-    google = db.Column(db.String(120))
-    linkedin = db.Column(db.String(120))
-    twitter = db.Column(db.String(120))
-    bitbucket = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                            onupdate=db.func.current_timestamp())
 
-    def __init__(self, email=None, password=None, display_name=None,
-                 facebook=None, github=None, google=None, linkedin=None,
-                 twitter=None, bitbucket=None):
+
+roles_parents = db.Table(
+    'roles_parents',
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
+    db.Column('parent_id', db.Integer, db.ForeignKey('role.id'))
+)
+
+
+@rbac.as_role_model
+class Role(Base, RoleMixin):
+    __tablename__ = 'auth_role'
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    description = db.Column(db.String(255))
+
+    def __init__(self, name):
+        self.name = name
+
+    parents = db.relationship(
+        'Role',
+        secondary=roles_parents,
+        primaryjoin=(id == roles_parents.c.role_id),
+        secondaryjoin=(id == roles_parents.c.parent_id),
+        backref=db.backref('children', lazy='dynamic')
+    )
+
+    def __init__(self, name):
+        RoleMixin.__init__(self)
+        self.name = name
+
+    def add_parent(self, parent):
+        # You don't need to add this role to parent's children set,
+        # relationship between roles would do this work automatically
+        self.parents.append(parent)
+
+    def add_parents(self, *parents):
+        for parent in parents:
+            self.add_parent(parent)
+
+    @staticmethod
+    def get_by_name(name):
+        return Role.query.filter_by(name=name).first()
+
+
+users_roles = db.Table(
+    'users_roles',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+)
+
+
+@rbac.as_user_model
+class User(Base, UserMixin):
+    __tablename__ = 'auth_user'
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    display_name = db.Column(db.String(120))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(db.String(45))
+    current_login_ip = db.Column(db.String(45))
+    login_count = db.Column(db.Integer)
+    # Other columns
+    roles = db.relationship(
+        'Role',
+        secondary=users_roles,
+        backref=db.backref('roles', lazy='dynamic')
+    )
+
+    def __init__(self, email=None, password=None, display_name=None):
         if email:
             self.email = email.lower()
         if password:
@@ -46,18 +106,25 @@ class User(db.Model):
         if display_name:
             self.display_name = display_name
 
-
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    def add_role(self, role):
+        self.roles.append(role)
+
+    def add_role(self, roles):
+        for role in roles:
+            self.add_role(role)
+
+    def get_roles(self):
+        for role in self.roles:
+            yield role
+
     def to_json(self):
-        return dict(id=self.id, email=self.email, displayName=self.display_name,
-                    facebook=self.facebook, google=self.google,
-                    linkedin=self.linkedin, twitter=self.twitter,
-                    bitbucket=self.bitbucket)
+        return dict(id=self.id, email=self.email, displayName=self.display_name)
 
 
 db.create_all()
@@ -104,14 +171,11 @@ def login_required(f):
     return decorated_function
 
 
+def get_current_user():
+    with curren_app.request_context():
+        return g.user_id
+
 # ------ Routes
-
-'''
-@app.route('/')
-def index():
-    return send_file(os.path.join(client_path, 'index.html'))
-'''
-
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
@@ -142,6 +206,7 @@ def signup():
     db.session.commit()
     token = create_token(user)
     return jsonify(token=token)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
